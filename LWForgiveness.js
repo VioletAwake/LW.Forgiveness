4cf835906800bad047a1e2ed695e66459735144d
VioletAwake <masked-goblin@hotmail.fr>
2024-06-13 20:40:44 +0200

    You want forgiveness ? Go to the church

--- LWForgiveness.js
+++ LWForgiveness.js
@@ -1,14 +1,14 @@
-const hiddenMessage ="TU VEUX TE PARDONNER ? ALORS VA A L'EGLISE !";
+const hiddenMessage = "TU VEUX TE PARDONNER ? ALORS VA A L'EGLISE !";
 const crosswordLetters = document.querySelectorAll(".letter");
 const backgroundMusic = document.getElementById("noir");
 const crossword = document.getElementById("crosswords-container");
 const card = document.getElementById("card");
 
-card.addEventListener('click', () => {
-  crossword.style.display = 'flex';
+card.addEventListener("click", () => {
+  crossword.style.display = "flex";
   backgroundMusic.play();
-  card.style.display= 'none';
-})
+  card.style.display = "none";
+});
 
 const filledLetters = [0, 1, 2, 13, 14, 17, 25, 27, 34];
 let errorCount = 0;
@@ -29,7 +29,6 @@ function checkLetterValidity(inputLetter, index) {
       hiddenLetters++;
     }
   }
-
 }
 
 crosswordLetters.forEach((letter) => {
@@ -64,8 +63,8 @@ function handleLetterInput(event) {
       `.letter[data-index="${nextIndex}"]`
     );
     while (nextLetter && nextLetter.textContent.trim() !== "") {
-        nextIndex++;
-        nextLetter = document.querySelector(`.letter[data-index="${nextIndex}"]`)
+      nextIndex++;
+      nextLetter = document.querySelector(`.letter[data-index="${nextIndex}"]`);
     }
     if (nextLetter) {
       nextLetter.focus();
@@ -78,17 +77,50 @@ function handleLetterInput(event) {
     backgroundMusic.pause();
 
     setTimeout(() => {
-        const knockDoor = document.getElementById("door");
-        knockDoor.volume = 1.0;
-        knockDoor.play();
+      const knockDoor = document.getElementById("door");
+      knockDoor.volume = 1.0;
+      knockDoor.play();
     }, 3000);
+
+    setTimeout(() => {
+      const videoForgiveness = document.getElementById("LWForgiveness");
+      videoForgiveness.style.display = "block";
+      videoForgiveness.play();
+      videoForgiveness.removeAttribute("controls");
+      videoForgiveness.addEventListener("pause", (event) => {
+        event.preventDefault();
+      });
+      videoForgiveness.onended = () => {
+        videoForgiveness.style.display = "none";
+        const videoToBeContinued = document.getElementById("ToBeContinued");
+        videoToBeContinued.style.display = "block";
+        videoToBeContinued.play();
+        videoToBeContinued.onplay = () => {
+          document.addEventListener("keydown", handleExit);
+          document.addEventListener("click", handleExit);
+        };
+      };
+    }, 7000);
   }
 }
 
 function allLettersFilled() {
-    return Array.from(crosswordLetters).every((letter) => letter.textContent.trim() !== "");
+  return Array.from(crosswordLetters).every(
+    (letter) => letter.textContent.trim() !== ""
+  );
 }
 
 function clearLetter(event) {
-    event.target.textContent = "";
+  event.target.textContent = "";
 }
+
+function handleExit(event) {
+  window.close();
+}
+
+const videos = document.querySelectorAll("video");
+videos.forEach((video) => {
+  video.addEventListener("contextmenu", (event) => {
+    event.preventDefault();
+  });
+});
